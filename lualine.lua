90af1a47b14d8ddb3e2492899129f8b4126366f0
Ostrichbeta Chan <ostrichb@outlook.com>
2026-06-05 07:59:36 +0000

    Slightly changes color theme for lualine

--- lualine.lua
+++ lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    theme = '16color',
+    theme = 'everforest',
     component_separators = {left = '', right = ''},
     section_separators = {left = '', right = ''},
     disabled_filetypes = {statusline = {}, winbar = {}},
